622909e8b5194e2aae8ea7a6c31bf2330f96fb7e
Avadhut Prabhudesai <avadhutprabhudesai23@gmail.com>
2022-02-10 15:27:31 +0530

    feat: #Template and Styles
    Added template and styles for button in the middle of the screen

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 import './style.css';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+/**
+ * CSS
+ *    - box shadow
+ *      - drop shadow vs inner shadow
+ *      - drop shadow vs inner shadow
+ *    - 3d effects using box shadow
+ *    - box shadow
+ *
+ */
